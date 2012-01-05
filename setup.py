4331e3407240b841f17f64ed664e7dd9626aca6a
Hugo Sena Ribeiro <hugosenari@gmail.com>
2012-01-05 01:07:59 -0200

    Updating setup for pypi

--- setup.py
+++ setup.py
@@ -9,10 +9,49 @@ from distutils.core import setup
 
 setup(name='pydbusdecorator',
       version='1.0',
-      description='Python decorator for dbus interface definition',
+      description='Python decorator for dbus interface client definition',
       author='hugosenari',
       author_email='hugosenari@gmail.com',
       url='https://github.com/hugosenari/pydbusdecorator',
+      keywords = ["dbus"],
       packages=('pydbusdecorator',),
       requires=('dbus',),
-     )
+      license = "GPL",
+      classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: X11 Applications",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+            "Programming Language :: Python :: 2.7",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      long_description = """\
+Python decorator for dbus interface client definition
+
+-----------------------------------------------------
+Python decorator for dbus interfaces
+
+For examples see tests (pydbusdecorator/tests) or mpris2 (https://github.com/hugosenari/mpris2)
+
+# importing
+from pydbusdecorator import DbusAttr, DbusInterface, DbusMethod
+
+# defining
+@DbusInterface('org.mpris.MediaPlayer2', '/org/mpris/MediaPlayer2')
+class Player(object):
+    @DbusMethod
+    def Next(self):
+        pass
+    @DbusAttr
+    def Volume(self):
+    pass
+    
+# using
+mediaplayer2 = Player(dbus_interface_info={'dbus_uri': 'org.mpris.MediaPlayer2.gmusicbrowser'})
+mediaplayer2.Next()
+print mediaplayer2.Volume
+mediaplayer2.Volume = 1
+print mediaplayer2.Volume # integer = 1 :P
+
+"""
+)
