216de872b92b47b45caa237ee9e787d60b702215
hugosenari <hugosenari@gmail.com>
2013-05-08 16:39:40 -0300

    Running tests from main

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,8 @@
 import unittest
+from dbus_attr import *
+from dbus_interface import *
+from dbus_method import *
+from extending import *
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
